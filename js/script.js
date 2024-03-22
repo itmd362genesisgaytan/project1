1787a7bb4a71170c6a126eee2a9c5b32278b333d
genesisgaytan00 <122740973+genesisgaytan00@users.noreply.github.com>
2024-03-21 22:13:26 -0500

    index.html
    fixed the form and added javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-
-    function showPopup() {
-        alert("Thank you for subscribing!");
+function onSubmit() {
+    var emailInput = document.getElementById("email").value;
+    if (emailInput.trim() === "") {
+        alert("Please enter a valid email address.");
+        return false;
     }
+    alert("Thank you for subscribing!");
+    return true;
+}
 
+    
